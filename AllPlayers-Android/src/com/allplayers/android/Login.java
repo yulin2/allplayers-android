c4c0876bdc7066038e0143faf70657b03de05ff5
Matthew Stephen <masta08@gmail.com>
2011-10-25 09:50:58 -0500

    added net.sf.json library

--- AllPlayers-Android/src/com/allplayers/android/Login.java
+++ AllPlayers-Android/src/com/allplayers/android/Login.java
@@ -1,6 +1,6 @@
 /* Copyright information */
 
-package allplayers.android;
+package com.allplayers.android;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,6 +14,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONSerializer;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /* Class information */
 public class Login extends Activity
@@ -70,7 +84,12 @@ public class Login extends Activity
 			public void onClick(View view)
 			{
 				TextView tv = new TextView(Login.this);
-				tv.setText("Hello, Android");
+				
+				String name = "";
+				String result = checkLogin(username.getText().toString(), password.getText().toString());
+				JSONArray jsonArray = (JSONArray) JSONSerializer.toJSON(result);
+				
+				tv.setText("Hello, " + name);
 				setContentView(tv);
 			}
 		});
@@ -86,5 +105,78 @@ public class Login extends Activity
 		panel.addView(loginButton);
 
 		return panel;
+	}
+	
+	public String checkLogin(String username, String password)
+	{
+		//Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager()
+		{
+			public java.security.cert.X509Certificate[] getAcceptedIssuers()
+			{
+			return null;
+			}
+			
+			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+			{
+			}
+			
+			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+			{
+			}
+		}};
+		
+		//Install the all-trusting trust manager
+		try
+		{
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		}
+		catch (Exception ex)
+		{
+		}
+		
+		//Now you can access an https URL without having the certificate in the truststore
+		
+		//Log in
+		try
+		{
+			URL url = new URL("https://www.allplayers.com/?q=api/v1/rest/users/login.json");
+			HttpURLConnection urlConn = (HttpURLConnection)url.openConnection();
+			DataOutputStream printout;
+			BufferedReader input;
+			
+			urlConn.setDoInput(true);
+			urlConn.setDoOutput(true);
+			urlConn.setUseCaches(false);
+			urlConn.setRequestMethod("POST");
+			urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			
+			//Send POST output.
+			printout = new DataOutputStream(urlConn.getOutputStream());
+			String content = "username=" + URLEncoder.encode(username, "UTF-8") + "&password=" + URLEncoder.encode(password, "UTF-8");
+			printout.writeBytes(content);
+			printout.flush();
+			printout.close();
+			
+			//Get response data.
+			input = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+			String str;
+			
+			String result = "";
+			while((str = input.readLine()) != null)
+			{
+				result += str;
+			}
+			
+			input.close();
+			return result;
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex);
+			return "";
+		}
 	}
 }
