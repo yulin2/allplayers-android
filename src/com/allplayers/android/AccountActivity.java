9069c100369ed60ce3e183d6e0d4089ce665231f
Matthew Stephen <masta08@gmail.com>
2011-11-08 00:05:49 -0600

    trying to fix tab pages

--- src/com/allplayers/android/AccountActivity.java
+++ src/com/allplayers/android/AccountActivity.java
@@ -1,8 +1,19 @@
 package com.allplayers.android;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class AccountActivity extends Activity
 {
-
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		TextView tv = new TextView(this);
+		tv.setText("Accounts Activity");
+		setContentView(tv);
+	}
 }
