aeb9f4db1935102708a4362bc6163c742f892ab7
Myles Martin <mylesmar10@gmail.com>
2011-11-19 03:23:31 -0600

    Got inbox WORKING!! Oh yeah! (It was hard :/ )

--- src/com/allplayers/android/MessagesMap.java
+++ src/com/allplayers/android/MessagesMap.java
@@ -1,26 +1,30 @@
 package com.allplayers.android;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MessagesMap
 {
 	private ArrayList<MessageData> mail = new ArrayList<MessageData>();
+	private String[] names;
 	
 	public MessagesMap(String jsonResult)
 	{
 		try
 		{
-			JSONArray jsonArray = new JSONArray(jsonResult);
-			//JSONObject jsonObject = new
+			JSONObject jsonObject = new JSONObject(jsonResult);
 			
-			if(jsonArray.length() > 0)
+			names = getNames(jsonObject);
+			
+			if(names.length > 0)
 			{
-				for(int i = 0; i < jsonResult.length(); i++)
+				for(int i = 0; i < names.length; i++)
 				{
-					mail.add(new MessageData(jsonArray.getString(i)));
+					mail.add(new MessageData(jsonObject.getString(names[i])));
 				}
 			}
 		}
@@ -28,6 +32,19 @@ public class MessagesMap
 		{
 			System.out.println(ex);
 		}
+	}
+	
+	private static String[] getNames( JSONObject jo ) {
+		int length = jo.length();
+		if (length == 0) return null;
+		Iterator iterator = jo.keys();
+		String[] names = new String[length];
+		int i = 0;
+		while (iterator.hasNext()) {
+			names[i] = (String)iterator.next();
+			i += 1;
+		}
+		return names;
 	}
 	
 	public ArrayList<MessageData> getMessageData()
