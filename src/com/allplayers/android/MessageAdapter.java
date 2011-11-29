1e1a7f875c8fc87a114bf1515f3adf7a0acacca2
Myles Martin <mylesmar10@gmail.com>
2011-11-29 01:18:34 -0600

    corrected tabbing

--- src/com/allplayers/android/MessageAdapter.java
+++ src/com/allplayers/android/MessageAdapter.java
@@ -7,21 +7,27 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class MessageAdapter extends ArrayAdapter<String> {
+public class MessageAdapter extends ArrayAdapter<String>
+{
 	private final Context context;
 	private final String[] senderNames;
-	//private final String[] dates;
-	//private final String[] subjects;
-	//private final String[] msgCounts;
+	private final String[] dates;
+	private final String[] subjects;
+	private final String[] msgCounts;
 
-	public MessageAdapter(Context context, String[] senderNames) {
+	public MessageAdapter(Context context, String[] senderNames, String[] dates, String[] subjects, String[] msgCounts) 
+	{
 		super(context, R.layout.custominbox, senderNames);
 		this.context = context;
 		this.senderNames = senderNames;
+		this.dates = dates;
+		this.subjects = subjects;
+		this.msgCounts = msgCounts;
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(int position, View convertView, ViewGroup parent) 
+	{
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View rowView = inflater.inflate(R.layout.custominbox, parent, false);
 		
