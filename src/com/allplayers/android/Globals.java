6c07353bdaa4d12e28ab54dc1e73e19292f652f4
Matt Stephen <masta08@gmail.com>
2011-12-05 21:38:01 -0600

    fixed tab/space consistencies and removed some old commented out code

--- src/com/allplayers/android/Globals.java
+++ src/com/allplayers/android/Globals.java
@@ -10,12 +10,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-//import java.io.BufferedInputStream; //used for first method of getting image
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
-//import java.net.URLConnection; //used for first method of getting image
 import java.util.ArrayList;
 
 /* This class is used for storing global variables across activities.
@@ -43,16 +41,6 @@ public class Globals
 	{
 		try
 		{
-			/*URL url = new URL(urlString);
-			final URLConnection conn = url.openConnection();
-			conn.connect();
-			final BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
-			final Bitmap bm = BitmapFactory.decodeStream(bis);
-			bis.close();
-			return bm;*/
-			
-			//Supposedly fixes images not displaying
-			//Did not work
 			HttpGet httpRequest = null;
 			
 			try
--- src/com/allplayers/android/MessageAdapter.java
+++ src/com/allplayers/android/MessageAdapter.java
@@ -11,55 +11,52 @@ import java.util.List;
 
 public class MessageAdapter extends BaseAdapter
 {
-    private Context context;
+	private Context context;
 
-    private List<MessageData> messageDataList;
+	private List<MessageData> messageDataList;
 
-    public MessageAdapter(Context context, List<MessageData> listMessageData) 
-    {
-        this.context = context;
-        messageDataList = listMessageData;
-    }
+	public MessageAdapter(Context context, List<MessageData> listMessageData) 
+	{
+		this.context = context;
+		messageDataList = listMessageData;
+	}
 
+	public View getView(int position, View convertView, ViewGroup viewGroup) 
+	{
+		MessageData entry = messageDataList.get(position);
+		if (convertView == null) 
+		{
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+					convertView = inflater.inflate(R.layout.inboxrow, null);
+		}
+		TextView senderName = (TextView) convertView.findViewById(R.id.senderName);
+		senderName.setText(entry.getLastSender());
 
-    public View getView(int position, View convertView, ViewGroup viewGroup) 
-    {
-        MessageData entry = messageDataList.get(position);
-        if (convertView == null) 
-        {
-            LayoutInflater inflater = (LayoutInflater) context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.inboxrow, null);
-        }
-        TextView senderName = (TextView) convertView.findViewById(R.id.senderName);
-        senderName.setText(entry.getLastSender());
+		TextView date = (TextView) convertView.findViewById(R.id.date);
+		date.setText(entry.getDateString());
 
-        TextView date = (TextView) convertView.findViewById(R.id.date);
-        date.setText(entry.getDateString());
+		TextView subject = (TextView) convertView.findViewById(R.id.subject);
+		subject.setText(entry.getSubject());
 
-        TextView subject = (TextView) convertView.findViewById(R.id.subject);
-        subject.setText(entry.getSubject());
-        
 		TextView body = (TextView) convertView.findViewById(R.id.body);
-        body.setText(entry.getMessageBody());
+		body.setText(entry.getMessageBody());
 
-        return convertView;
-    }
+		return convertView;
+	}
 
+	public int getCount() 
+	{
+		return messageDataList.size();
+	}
 
-    public int getCount() 
-    {
-        return messageDataList.size();
-    }
+	public Object getItem(int position) 
+	{
+		return messageDataList.get(position);
+	}
 
-    public Object getItem(int position) 
-    {
-        return messageDataList.get(position);
-    }
-
-    public long getItemId(int position) 
-    {
-        return position;
-    }
-
+	public long getItemId(int position) 
+	{
+		return position;
+	}
 }
