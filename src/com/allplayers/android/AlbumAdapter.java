4ff7fc9581ed204f5c5e2678d4ecfebd14482fbb
Ian Cowley <icowley@allplayers.com>
2013-01-04 14:47:39 -0600

    Made thread changes.

--- src/com/allplayers/android/AlbumAdapter.java
+++ src/com/allplayers/android/AlbumAdapter.java
@@ -1,73 +1,89 @@
 package com.allplayers.android;
-
+ 
 import com.allplayers.objects.AlbumData;
 import com.allplayers.rest.RestApiV1;
-
+ 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+ 
 import java.util.ArrayList;
 import java.util.List;
-
+ 
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
 public class AlbumAdapter extends ArrayAdapter<AlbumData> {
     private ImageView albumCoverPhoto;
     private TextView albumTitle;
     private TextView albumExtraInfo;
     private List<AlbumData> albums = new ArrayList<AlbumData>();
-
+ 
     public AlbumAdapter(Context context, int textViewResourceId, List<AlbumData> objects) {
         super(context, textViewResourceId, objects);
         this.albums = objects;
     }
-
+ 
     public int getCount() {
         return albums.size();
     }
-
+ 
     public AlbumData getItem(int index) {
         return albums.get(index);
     }
-
+ 
     public View getView(int position, View convertView, ViewGroup parent) {
         View row = convertView;
-
+ 
         if (row == null) {
             //Row Inflation
             LayoutInflater inflater = (LayoutInflater)this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             row = inflater.inflate(R.layout.albumlistitem, parent, false);
         }
-
+ 
         //Get item
         AlbumData album = getItem(position);
-
+ 
         //Get reference to ImageView
         albumCoverPhoto = (ImageView)row.findViewById(R.id.albumCoverPhoto);
-
+ 
         //Get reference to TextView - albumTitle
         albumTitle = (TextView)row.findViewById(R.id.albumTitle);
-
+ 
         //Get reference to TextView - albumExtraInfo
         albumExtraInfo = (TextView)row.findViewById(R.id.albumExtraInfo);
-
+ 
         //Set album title
         albumTitle.setText(album.getTitle());
-
+ 
         //Set cover photo icon
         String imageURL = album.getCoverPhoto();
-
+ 
         if (!imageURL.trim().equals("")) {
-            Bitmap bitmap = RestApiV1.getRemoteImage(album.getCoverPhoto());
-            albumCoverPhoto.setImageBitmap(bitmap);
+            CoverPhotoTask helper = new CoverPhotoTask();
+        	  helper.execute(album);
         }
-
+ 
         //Set extra info
         albumExtraInfo.setText("");
         return row;
     }
+    public class CoverPhotoTask extends AsyncTask<AlbumData, Void, Bitmap> {
+    	 
+        protected Bitmap doInBackground(AlbumData... albums) {
+        	return RestApiV1.getRemoteImage(albums[0].getCoverPhoto());
+        }
+         
+ 		protected void onPostExecute(Bitmap bitmap) {
+ 			albumCoverPhoto.setImageBitmap(bitmap);
+     	}
+     }
 }
--- src/com/allplayers/android/EventsActivity.java
+++ src/com/allplayers/android/EventsActivity.java
@@ -1,53 +1,70 @@
 package com.allplayers.android;
-
+ 
+import com.allplayers.objects.AlbumData;
 import com.allplayers.objects.EventData;
-
+ 
 import com.allplayers.rest.RestApiV1;
-
+ 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-
+import android.widget.Toast;
+ 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+ 
 public class EventsActivity extends ListActivity {
     private ArrayList<EventData> eventsList;
     private ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>(2);
     private boolean hasEvents = false;
-
+    private String jsonResult;
+ 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String jsonResult;
-
+ 
         //check local storage
         if (LocalStorage.getTimeSinceLastModification("UserEvents") / 1000 / 60 < 10) { //more recent than 10 minutes
             jsonResult = LocalStorage.readUserEvents(getBaseContext());
+            setEventsMap();
         } else {
-            jsonResult = RestApiV1.getUserEvents();
-            LocalStorage.writeUserEvents(getBaseContext(), jsonResult, false);
+            //jsonResult = RestApiV1.getUserEvents();
+          GetUserEventsTask helper = new GetUserEventsTask();
+        	helper.execute();
         }
-
-        EventsMap events = new EventsMap(jsonResult);
+    }
+ 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+ 
+        if (hasEvents) {
+            // Can be used to display map or full details.
+            Intent intent = (new Router(this)).getEventDisplayActivityIntent(eventsList.get(position));
+            startActivity(intent);
+        }
+    }
+    
+    protected void setEventsMap() {
+    	EventsMap events = new EventsMap(jsonResult);
         eventsList = events.getEventData();
         HashMap<String, String> map;
-
+ 
         if (!eventsList.isEmpty()) {
             for (int i = 0; i < eventsList.size(); i++) {
                 map = new HashMap<String, String>();
                 map.put("line1", eventsList.get(i).getTitle());
-
+ 
                 String start = eventsList.get(i).getStartDateString();
                 map.put("line2", start);
                 list.add(map);
             }
-
+ 
             hasEvents = true;
         } else {
             map = new HashMap<String, String>();
@@ -56,23 +73,25 @@ public class EventsActivity extends ListActivity {
             list.add(map);
             hasEvents = false;
         }
-
+ 
         String[] from = {"line1", "line2"};
-
+ 
         int[] to = {android.R.id.text1, android.R.id.text2};
-
+ 
         SimpleAdapter adapter = new SimpleAdapter(this, list, android.R.layout.simple_list_item_2, from, to);
         setListAdapter(adapter);
     }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        if (hasEvents) {
-            // Can be used to display map or full details.
-            Intent intent = (new Router(this)).getEventDisplayActivityIntent(eventsList.get(position));
-            startActivity(intent);
+    
+    public class GetUserEventsTask extends AsyncTask<Void, Void, String> {
+   	 
+        protected String doInBackground(Void... args) {
+        	return RestApiV1.getUserEvents();
         }
-    }
+        
+        protected void onPostExecute(String jsonResult) {
+        	EventsActivity.this.jsonResult = jsonResult;
+        	LocalStorage.writeUserEvents(getBaseContext(), jsonResult, false);
+        	setEventsMap();
+        }
+     }
 }
