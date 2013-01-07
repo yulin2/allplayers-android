e148e6fdfcfd62793f2b47d680af6b65324c1f4d
Jonathan Darling <jdarling@allplayers.com>
2013-01-07 10:58:35 -0600

    Removed old code from GroupAlbumsActivity.java.

--- src/com/allplayers/android/GroupAlbumsActivity.java
+++ src/com/allplayers/android/GroupAlbumsActivity.java
@@ -27,21 +27,6 @@ public class GroupAlbumsActivity  extends ListActivity {
         
         GetGroupAlbumsByGroupIdTask helper = new GetGroupAlbumsByGroupIdTask();
         helper.execute(group);
-        
-//        String jsonResult = RestApiV1.getGroupAlbumsByGroupId(group.getUUID());
-//        AlbumsMap albums = new AlbumsMap(jsonResult);
-//        albumList = albums.getAlbumData();
-//
-//        if (albumList.isEmpty()) {
-//            String[] values = new String[] {"no albums to display"};
-//            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-//                    android.R.layout.simple_list_item_1, values);
-//            setListAdapter(adapter);
-//        } else {
-//            //Create a customized ArrayAdapter
-//            AlbumAdapter adapter = new AlbumAdapter(getApplicationContext(), R.layout.albumlistitem, albumList);
-//            setListAdapter(adapter);
-//        }
     }
 
     @Override
