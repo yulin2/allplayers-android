3b524e547f6259941599457fd18114a62947747b
Jonathan Darling <jdarling@allplayers.com>
2013-01-09 11:16:56 -0600

    Got rid of the get() call to the AsyncTask and moved the code into onPostExecute().

--- src/com/allplayers/android/MessageInbox.java
+++ src/com/allplayers/android/MessageInbox.java
@@ -37,51 +37,43 @@ public class MessageInbox extends Activity {
         if (jsonResult.equals("")) {
         	GetUserInboxTask helper = new GetUserInboxTask();
         	helper.execute();
-        	
-        	// helper.get() necessary because the AsyncTask needs to finish before the main thread can continue. 
-        	try {
-				helper.get();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
         }
-
-        MessagesMap messages = new MessagesMap(jsonResult);
-        messageList = messages.getMessageData();
-
-        Collections.reverse(messageList);
-
-        ListView list = (ListView) findViewById(R.id.customListView);
-        list.setClickable(true);
-
-        if (!messageList.isEmpty()) {
-            hasMessages = true;
-        } else {
-            hasMessages = false;
-        }
-
-        final List<MessageData> messageList2 = messageList;
-        MessageAdapter adapter = new MessageAdapter(this, messageList2);
-
-        list.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
-                if (hasMessages) {
-                    // Go to the message thread.
-                    Intent intent = (new Router(MessageInbox.this)).getMessageThreadIntent(messageList.get(position));
-                    startActivity(intent);
-                }
-            }
-        });
-
-        list.setAdapter(adapter);
     }
     
-    public class GetUserInboxTask extends AsyncTask<Void, Void, Void> {
-    	protected Void doInBackground(Void... Args) {
-        	MessageInbox.this.jsonResult = RestApiV1.getUserInbox();
-        	return null;
+    public class GetUserInboxTask extends AsyncTask<Void, Void, String> {
+    	protected String doInBackground(Void... Args) {
+        	return jsonResult = RestApiV1.getUserInbox();
+    	}
+    	
+    	protected void onPostExecute(String jsonResult) {
+    		 MessagesMap messages = new MessagesMap(jsonResult);
+    	        messageList = messages.getMessageData();
+
+    	        Collections.reverse(messageList);
+
+    	        ListView list = (ListView) findViewById(R.id.customListView);
+    	        list.setClickable(true);
+
+    	        if (!messageList.isEmpty()) {
+    	            hasMessages = true;
+    	        } else {
+    	            hasMessages = false;
+    	        }
+
+    	        final List<MessageData> messageList2 = messageList;
+    	        MessageAdapter adapter = new MessageAdapter(MessageInbox.this, messageList2);
+
+    	        list.setOnItemClickListener(new OnItemClickListener() {
+    	            public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
+    	                if (hasMessages) {
+    	                    // Go to the message thread.
+    	                    Intent intent = (new Router(MessageInbox.this)).getMessageThreadIntent(messageList.get(position));
+    	                    startActivity(intent);
+    	                }
+    	            }
+    	        });
+
+    	        list.setAdapter(adapter);
     	}
     }
 }
