b5f246c42d4ea6fd3d53813dbf1e26ddfd9c343d
Jonathan Darling <jdarling@allplayers.com>
2013-01-09 09:38:06 -0600

    Moved REST call to AsyncTask Thread in PhotosActivity.java.

--- src/com/allplayers/android/PhotosActivity.java
+++ src/com/allplayers/android/PhotosActivity.java
@@ -6,12 +6,14 @@ import com.allplayers.rest.RestApiV1;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class PhotosActivity  extends ListActivity {
     private ArrayList<AlbumData> albumList = new ArrayList<AlbumData>();
@@ -45,47 +47,13 @@ public class PhotosActivity  extends ListActivity {
             //check local storage
             if (LocalStorage.getTimeSinceLastModification("UserGroups") / 1000 / 60 < 60) { //more recent than 60 minutes
                 jsonResult = LocalStorage.readUserGroups(getBaseContext());
+                populateGroupAlbums(jsonResult);
             } else {
-                jsonResult = RestApiV1.getUserGroups();
-                LocalStorage.writeUserGroups(getBaseContext(), jsonResult, false);
-            }
-
-            GroupsMap groups = new GroupsMap(jsonResult);
-            ArrayList<GroupData> groupList = groups.getGroupData();
-
-            if (!groupList.isEmpty()) {
-                String group_uuid;
-                AlbumsMap albums;
-                ArrayList<AlbumData> newAlbumList;
-
-                LocalStorage.writeUserAlbums(getBaseContext(), "", true);
-
-                for (int i = 0; i < groupList.size(); i++) {
-                    group_uuid = groupList.get(i).getUUID();
-                    jsonResult = RestApiV1.getGroupAlbumsByGroupId(group_uuid);
-                    LocalStorage.appendUserAlbums(getBaseContext(), jsonResult);
-                    albums = new AlbumsMap(jsonResult);
-                    newAlbumList = albums.getAlbumData();
-
-                    if (!newAlbumList.isEmpty()) {
-                        for (int j = 0; j < newAlbumList.size(); j++) {
-                            albumList.add(newAlbumList.get(j));
-                        }
-                    }
-                }
+            	GetUserGroupsTask helper = new GetUserGroupsTask();
+            	helper.execute();
             }
         }
 
-        if (albumList.isEmpty()) {
-            String[] values = new String[] {"no albums to display"};
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1, values);
-            setListAdapter(adapter);
-        } else {
-            //Create a customized ArrayAdapter
-            AlbumAdapter adapter = new AlbumAdapter(getApplicationContext(), R.layout.albumlistitem, albumList);
-            setListAdapter(adapter);
-        }
     }
 
     @Override
@@ -97,5 +65,69 @@ public class PhotosActivity  extends ListActivity {
             Intent intent = (new Router(this)).getAlbumPhotosActivityIntent(albumList.get(position));
             startActivity(intent);
         }
+    }
+    
+    protected void populateGroupAlbums(String jsonResult) {
+    	GroupsMap groups = new GroupsMap(jsonResult);
+        ArrayList<GroupData> groupList = groups.getGroupData();
+
+        if (!groupList.isEmpty()) {
+            String group_uuid;
+            AlbumsMap albums;
+            ArrayList<AlbumData> newAlbumList;
+
+            LocalStorage.writeUserAlbums(getBaseContext(), "", true);
+
+            for (int i = 0; i < groupList.size(); i++) {
+                group_uuid = groupList.get(i).getUUID();
+                GetGroupAlbumsByGroupIdTask helper = new GetGroupAlbumsByGroupIdTask();
+                helper.execute(group_uuid);
+                try {
+					jsonResult = helper.get();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+                LocalStorage.appendUserAlbums(getBaseContext(), jsonResult);
+                albums = new AlbumsMap(jsonResult);
+                newAlbumList = albums.getAlbumData();
+
+                if (!newAlbumList.isEmpty()) {
+                    for (int j = 0; j < newAlbumList.size(); j++) {
+                        albumList.add(newAlbumList.get(j));
+                    }
+                }
+            }
+        }
+    }
+    
+    public class GetUserGroupsTask extends AsyncTask<Void, Void, String> {
+    	
+    	protected String doInBackground(Void... args) {
+    		return RestApiV1.getUserGroups();
+    	}
+    	
+    	protected void onPostExecute(String jsonResult) {
+            LocalStorage.writeUserGroups(getBaseContext(), jsonResult, false);
+            populateGroupAlbums(jsonResult);
+            if (albumList.isEmpty()) {
+                String[] values = new String[] {"no albums to display"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(PhotosActivity.this,
+                        android.R.layout.simple_list_item_1, values);
+                setListAdapter(adapter);
+            } else {
+                //Create a customized ArrayAdapter
+                AlbumAdapter adapter = new AlbumAdapter(getApplicationContext(), R.layout.albumlistitem, albumList);
+                setListAdapter(adapter);
+            }
+    	}
+    }
+    
+    public class GetGroupAlbumsByGroupIdTask extends AsyncTask<String, Void, String> {
+    	
+    	protected String doInBackground(String... group_uuid) {
+    		return RestApiV1.getGroupAlbumsByGroupId(group_uuid[0]);
+    	}
     }
 }
