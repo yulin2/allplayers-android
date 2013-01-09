f4bfcf620d805a5a0e2d1b605c7bf79c67ed5176
Jonathan Darling <jdarling@allplayers.com>
2013-01-09 12:57:44 -0600

    Cleaned up some code.

--- src/com/allplayers/android/PhotoDisplayActivity.java
+++ src/com/allplayers/android/PhotoDisplayActivity.java
@@ -35,13 +35,6 @@ public class PhotoDisplayActivity extends Activity implements OnTouchListener {
         
         GetRemoteImageOnTouchTask helper = new GetRemoteImageOnTouchTask();
         helper.execute(photoUrl);
-//        try {
-//			helper.get();
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		} catch (ExecutionException e) {
-//			e.printStackTrace();
-//		}
     }
 
     public boolean onTouch(View arg0, MotionEvent arg1) {
@@ -133,4 +126,4 @@ public class PhotoDisplayActivity extends Activity implements OnTouchListener {
             imView.setImageBitmap(image);
     	}
     }
-}
+}  
