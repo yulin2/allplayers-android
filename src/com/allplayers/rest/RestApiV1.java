741271f0fac6e5042f1983be35ca366de3e4bce4
Ian Cowley <icowley@allplayers.com>
2013-01-14 14:11:30 -0600

    Fixed the group search so it does not give a blank search term.

--- src/com/allplayers/rest/RestApiV1.java
+++ src/com/allplayers/rest/RestApiV1.java
@@ -147,14 +147,20 @@ public class RestApiV1 {
     }
 
     public static String searchGroups(String search, int zipcode, int distance) {
-        return makeUnauthenticatedGet("https://www.allplayers.com/?q=api/v1/rest/groups.json&search=\""
-                                      + search
-                                      + "\""
-                                      + "&distance[postal_code]="
-                                      + zipcode
-                                      + "&distance[search_distance]="
-                                      + distance
-                                      + "&distance[search_units]=mile");
+        String searchTerms = "https://www.allplayers.com/?q=api/v1/rest/groups.json";
+            if(search.length() != 0) {
+                searchTerms += ("&search=\"" + search + "\"");
+            }
+            // As of right now, the input distance will only matter if a zipcode is given, 
+            // so it is only considered in that case.
+            // TODO Add in considering the distance as "Distance from my location"
+            if(zipcode != 0) {
+                searchTerms += ("&distance[postal_code]=" + zipcode
+                                + "&distance[search_distance]="
+                                + distance
+                                + "&distance[search_units]=mile");
+            }
+        return makeUnauthenticatedGet(searchTerms);
     }
 
     public static String getUserGroups() {
