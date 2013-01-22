527b273e08e384c9f42efd9cef63bbb32f0fb0bc
Ian Cowley <icowley@allplayers.com>
2013-01-22 11:46:13 -0600

    Removed unused imports.

--- src/com/allplayers/android/GroupsActivity.java
+++ src/com/allplayers/android/GroupsActivity.java
@@ -11,10 +11,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
