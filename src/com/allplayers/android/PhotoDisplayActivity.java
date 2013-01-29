1598b7b6caae8b9b0fb5731434ed063462386bea
Ian Cowley <icowley@allplayers.com>
2013-01-29 15:51:09 -0600

    Have some decent work on lazy photo loading done.

--- src/com/allplayers/android/PhotoDisplayActivity.java
+++ src/com/allplayers/android/PhotoDisplayActivity.java
@@ -1,5 +1,6 @@
 package com.allplayers.android;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 import com.allplayers.objects.PhotoData;
@@ -18,6 +19,9 @@ import android.widget.ViewFlipper;
 
 public class PhotoDisplayActivity extends Activity implements OnTouchListener {
     float  downYValue;
+    private ViewFlipper slider;
+    private ArrayList<ImageView> photos;
+    private int currentPhoto = 1;
 
     /**
      * The current photo displayed.
@@ -30,11 +34,31 @@ public class PhotoDisplayActivity extends Activity implements OnTouchListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photodisplay);
 
+        slider = (ViewFlipper)findViewById(R.id.Slider);
+        slider.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+        slider.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
+
+        photos = new ArrayList<ImageView>();
+        // Create ImageViews and set their on touch listeners.
+        photos.add((ImageView)findViewById(R.id.firstPhoto));
+        photos.add((ImageView)findViewById(R.id.secondPhoto));
+        photos.add((ImageView)findViewById(R.id.thirdPhoto));
+        photos.get(0).setOnTouchListener((OnTouchListener) PhotoDisplayActivity.this);
+        photos.get(1).setOnTouchListener((OnTouchListener) PhotoDisplayActivity.this);
+        photos.get(2).setOnTouchListener((OnTouchListener) PhotoDisplayActivity.this);
+        slider.setDisplayedChild(1);
+
         mPhoto = (new Router(this)).getIntentPhoto();
         String photoUrl = mPhoto.getPhotoFull();
 
-        GetRemoteImageOnTouchTask helper = new GetRemoteImageOnTouchTask();
-        helper.execute(photoUrl);
+        // Set the initial 3 images with the current one in the center.
+        new GetRemoteImageTask().execute(photoUrl, 1);
+        if (mPhoto.nextPhoto() != null) {
+            new GetRemoteImageTask().execute(mPhoto.nextPhoto().getPhotoFull(), 2);
+        }
+        if (mPhoto.previousPhoto() != null) {
+            new GetRemoteImageTask().execute(mPhoto.previousPhoto().getPhotoFull(), 0);
+        }
     }
 
     public boolean onTouch(View arg0, MotionEvent arg1) {
@@ -48,37 +72,36 @@ public class PhotoDisplayActivity extends Activity implements OnTouchListener {
             float currentX = arg1.getX();
 
             if (downYValue < currentX) {
-                ViewFlipper slider = (ViewFlipper)findViewById(R.id.Slider);
-                slider.setAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
-                slider.setAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
 
                 //change image
                 if (mPhoto.previousPhoto() != null) {
                     mPhoto = mPhoto.previousPhoto();
+                    PhotoData photoToLoad = mPhoto.previousPhoto();
+                    GetRemoteImageTask helper = new GetRemoteImageTask();
+                    if (mPhoto.previousPhoto() != null) {
+                        helper.execute(photoToLoad.getPhotoFull(), currentPhoto + 1);
+                    }
+                    currentPhoto--;
+                    if (currentPhoto == -1) currentPhoto = 2;
+                    slider.setDisplayedChild(currentPhoto);
                 }
 
-                PhotoData photo = mPhoto;
-                String photoUrl = photo.getPhotoFull();
 
-                GetRemoteImageTask helper = new GetRemoteImageTask();
-                helper.execute(photoUrl);
             }
 
             if (downYValue > currentX) {
-                ViewFlipper slider = (ViewFlipper) findViewById(R.id.Slider);
-                slider.setAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
-                slider.setAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
 
                 //change image
                 if (mPhoto.nextPhoto() != null) {
                     mPhoto = mPhoto.nextPhoto();
+                    PhotoData photoToLoad = mPhoto.nextPhoto();
+                    GetRemoteImageTask helper = new GetRemoteImageTask();
+                    if (mPhoto.nextPhoto() != null)
+                        helper.execute(photoToLoad.getPhotoFull(), currentPhoto - 1);
+                    currentPhoto++;
+                    if (currentPhoto == 3) currentPhoto = 0;
+                    slider.setDisplayedChild(currentPhoto);
                 }
-
-                PhotoData photo = mPhoto;
-                String photoUrl = photo.getPhotoFull();
-
-                GetRemoteImageTask helper = new GetRemoteImageTask();
-                helper.execute(photoUrl);
             }
 
             break;
@@ -91,31 +114,15 @@ public class PhotoDisplayActivity extends Activity implements OnTouchListener {
     /*
      * Get's a user's image using a rest call and displays it.
      */
-    public class GetRemoteImageOnTouchTask extends AsyncTask<String, Void, Bitmap> {
-
-        protected Bitmap doInBackground(String... photoUrl) {
-            return RestApiV1.getRemoteImage(photoUrl[0]);
+    public class GetRemoteImageTask extends AsyncTask<Object, Void, Bitmap> {
+        int index;
+        protected Bitmap doInBackground(Object... photoUrl) {
+            index = ((Integer) photoUrl[1]);
+            return RestApiV1.getRemoteImage((String) photoUrl[0]);
         }
 
         protected void onPostExecute(Bitmap image) {
-            ImageView imView = (ImageView)findViewById(R.id.fullPhotoDisplay);
-            imView.setImageBitmap(image);
-            imView.setOnTouchListener((OnTouchListener) PhotoDisplayActivity.this);
-        }
-    }
-
-    /*
-     * Get's a user's image using a rest call and displays it.
-     */
-    public class GetRemoteImageTask extends AsyncTask<String, Void, Bitmap> {
-
-        protected Bitmap doInBackground(String... photoUrl) {
-            return RestApiV1.getRemoteImage(photoUrl[0]);
-        }
-
-        protected void onPostExecute(Bitmap image) {
-            ImageView imView = (ImageView)findViewById(R.id.fullPhotoDisplay);
-            imView.setImageBitmap(image);
+            photos.get(index).setImageBitmap(image);
         }
     }
 }
