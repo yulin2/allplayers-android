20cfc35d853c7493e6d1c700c28847c6233a1e95
Ian Cowley <icowley@allplayers.com>
2013-01-30 16:31:42 -0600

    Add all images to a gridview that will load all images asyncly.

--- src/com/allplayers/android/AlbumPhotosActivity.java
+++ src/com/allplayers/android/AlbumPhotosActivity.java
@@ -4,59 +4,62 @@ import com.allplayers.rest.RestApiV1;
 import com.allplayers.objects.AlbumData;
 import com.allplayers.objects.PhotoData;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class AlbumPhotosActivity extends ListActivity {
+public class AlbumPhotosActivity extends Activity {
     private ArrayList<PhotoData> photoList;
-    private ArrayAdapter adapter = null;
+    private ArrayAdapter blankAdapter = null;
+    private PhotoAdapter photoAdapter;
     private int currentPage;
     private int currentAmountShown;
+    private GridView grid;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.albumdisplay);
         AlbumData album = (new Router(this)).getIntentAlbum();
         photoList = new ArrayList<PhotoData>();
+        grid = (GridView) findViewById(R.id.gridview);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                if (!photoList.isEmpty()) {
+                    // Display the group page for the selected group
+                    Intent intent = (new Router(AlbumPhotosActivity.this)).getPhotoDisplayActivityIntent(photoList.get(position));
+                    startActivity(intent);
+                }
+            }
+        });
         GetAlbumPhotosByAlbumIdTask helper = new GetAlbumPhotosByAlbumIdTask();
         helper.execute(album);
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        if (!photoList.isEmpty()) {
-            // Display the group page for the selected group
-            Intent intent = (new Router(this)).getPhotoDisplayActivityIntent(photoList.get(position));
-            startActivity(intent);
-        }
-    }
-
     public void setAdapter() {
-        if (adapter == null) {
-            if (photoList.isEmpty()) {
-                String[] values = new String[] {"no photos to display"};
+        if (photoList.isEmpty()) {
+            String[] values = new String[] {"no photos to display"};
 
-                adapter = new ArrayAdapter<String>(AlbumPhotosActivity.this,
-                                                   android.R.layout.simple_list_item_1, values);
-                setListAdapter(adapter);
-            } else {
-                //Create a customized ArrayAdapter
-                adapter = new PhotoAdapter(getApplicationContext(), R.layout.photolistitem);
-                setListAdapter(adapter);
-            }
+            blankAdapter = new ArrayAdapter<String>(AlbumPhotosActivity.this,
+                                                    android.R.layout.simple_list_item_1, values);
+            grid.setAdapter(blankAdapter);
+        } else {
+            //Create a customized ArrayAdapter
+            photoAdapter = new PhotoAdapter(getApplicationContext());
+            grid.setAdapter(photoAdapter);
         }
     }
 
@@ -65,7 +68,7 @@ public class AlbumPhotosActivity extends ListActivity {
         photoList.addAll(photos.getPhotoData());
         setAdapter();
         for (int i = currentAmountShown; i < photoList.size(); i++, currentAmountShown++) {
-            adapter.add(photoList.get(i));
+            photoAdapter.add(photoList.get(i));
         }
     }
 
@@ -75,7 +78,7 @@ public class AlbumPhotosActivity extends ListActivity {
     public class GetAlbumPhotosByAlbumIdTask extends AsyncTask<AlbumData, Void, String> {
 
         protected String doInBackground(AlbumData... album) {
-            return RestApiV1.getAlbumPhotosByAlbumId(album[0].getUUID(), currentPage++ * 5, 5);
+            return RestApiV1.getAlbumPhotosByAlbumId(album[0].getUUID(), 0, 0);
         }
 
         protected void onPostExecute(String jsonResult) {
--- src/com/allplayers/android/PhotoAdapter.java
+++ src/com/allplayers/android/PhotoAdapter.java
@@ -13,22 +13,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PhotoAdapter extends ArrayAdapter<PhotoData> {
+public class PhotoAdapter extends BaseAdapter {
     private List<ImageView> photoImage = new ArrayList<ImageView>();
     private TextView photoTitle;
     private TextView photoExtraInfo;
     private List<PhotoData> photos = new ArrayList<PhotoData>();
+    private Context mContext;
+    private int currentImage = 0;
 
-    public PhotoAdapter(Context context, int textViewResourceId, List<PhotoData> objects) {
-        super(context, textViewResourceId, objects);
+    public PhotoAdapter(Context context, List<PhotoData> objects) {
+        mContext = context;
         this.photos = objects;
     }
 
-    public PhotoAdapter(Context context, int textViewResourceId) {
-        super(context, textViewResourceId);
+    public PhotoAdapter(Context context) {
+        mContext = context;
     }
 
     public void add(PhotoData photo) {
@@ -48,40 +52,29 @@ public class PhotoAdapter extends ArrayAdapter<PhotoData> {
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-
-        if (row == null) {
-            //Row Inflation
-            LayoutInflater inflater = (LayoutInflater)this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = inflater.inflate(R.layout.photolistitem, parent, false);
-        }
+        ImageView image;
+        image = new ImageView(mContext);
+        image.setLayoutParams(new GridView.LayoutParams(140, 140));
+        image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        image.setPadding(5, 5, 5, 5);
 
         //Get item
         PhotoData photo = getItem(position);
 
         //Get reference to ImageView
-        photoImage.add(position, (ImageView)row.findViewById(R.id.photoImage));
-
-        //Get reference to TextView - albumTitle
-        photoTitle = (TextView)row.findViewById(R.id.photoTitle);
-
-        //Get reference to TextView - albumExtraInfo
-        photoExtraInfo = (TextView)row.findViewById(R.id.photoExtraInfo);
-
-        //Set album title
-        photoTitle.setText(photo.getTitle());
+        photoImage.add(currentImage, image);
 
         //Set cover photo icon
         String imageURL = photo.getPhotoThumb();
+        System.out.println("Getting image for position " + position + "\nAnd we are on currentImage = " + currentImage);
+        System.out.println(imageURL);
 
         if (!imageURL.trim().equals("")) {
             GetRemoteImageTask helper = new GetRemoteImageTask();
-            helper.execute(photo, position);
+            helper.execute(photo, currentImage);
         }
 
-        //Set extra info
-        photoExtraInfo.setText("");
-        return row;
+        return photoImage.get(currentImage++);
     }
 
     /*
@@ -98,5 +91,10 @@ public class PhotoAdapter extends ArrayAdapter<PhotoData> {
         protected void onPostExecute(Bitmap bitmap) {
             photoImage.get(row).setImageBitmap(bitmap);
         }
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        return 0;
     }
 }
