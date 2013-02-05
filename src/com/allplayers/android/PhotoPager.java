ca564de921b655c46c4a693922ee6c5d710f7ae5
Ian Cowley <icowley@allplayers.com>
2013-02-05 15:28:27 -0600

    Added caching to the photo display.

--- src/com/allplayers/android/PhotoPager.java
+++ src/com/allplayers/android/PhotoPager.java
@@ -7,12 +7,14 @@ import com.allplayers.android.PhotoDisplayActivity.GetRemoteImageTask;
 import com.allplayers.objects.PhotoData;
 import com.allplayers.rest.RestApiV1;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.LruCache;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
@@ -20,32 +22,47 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class PhotoPager extends Activity {
+	private LruCache<String, Bitmap> mMemoryCache;
     private ViewPager mViewPager;
     private PhotoPagerAdapter photoAdapter;
     private PhotoData currentPhoto;
+	int currentPhotoIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @SuppressLint("NewApi")
+			@Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than
+                // number of items.
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+        
         currentPhoto = (new Router(this)).getIntentPhoto();
         mViewPager = new ViewPager(this);
         setContentView(mViewPager);
         photoAdapter = new PhotoPagerAdapter(this, currentPhoto);
         mViewPager.setAdapter(photoAdapter);
+        mViewPager.setCurrentItem(currentPhotoIndex);
     }
 
 
     public class PhotoPagerAdapter extends PagerAdapter {
     	private Context mContext;
-    	int currentPhotoIndex;
-        private List<ImageView> images;
+        private ImageView[] images;
         private List<PhotoData> photos;
-        
+
         public PhotoPagerAdapter(Context context, PhotoData item) {
             mContext = context;
             photos = new ArrayList<PhotoData>();
-            images = new ArrayList<ImageView>();
            
             PhotoData temp = item;
             while(temp.previousPhoto() != null) {
@@ -53,7 +70,7 @@ public class PhotoPager extends Activity {
             	temp = temp.previousPhoto();
             	System.out.println("Added a photo before.");
             }
-            int currentPhotoIndex = photos.size();
+            currentPhotoIndex = photos.size();
             photos.add(item);
             temp = item;
             while(temp.nextPhoto() != null) {
@@ -61,42 +78,24 @@ public class PhotoPager extends Activity {
             	temp = temp.nextPhoto();
             	System.out.println("Added a photo after.");
             }
-            
-            mViewPager.setCurrentItem(currentPhotoIndex);
+            System.out.println("Current Photo Index = " + currentPhotoIndex);
+            images = new ImageView[photos.size()];
         }
-
-        /*public PhotoPagerAdapter(PhotoPagerAdapter orig, int newCenter) {
-            images = orig.images;
-            photos = orig.photos;
-            mContext = orig.mContext;
-            ImageView image = new ImageView(mContext);
-            switch (newCenter) {
-            	// Remove the end image and add a new blank image to the beginning.
-            	case 0: {
-            		images.remove(2);
-            		images.add(0, image);
-            		photos.remove(2);
-            		photos.add(0, photos.get(0).previousPhoto());
-            		new GetRemoteImageTask().execute(photos.get(0).getPhotoFull(), 0);
-            	}
-            	// Remove the first image and add a new blank image to the end.
-            	case 2: {
-            		images.remove(0);
-            		images.add(image);
-            		photos.remove(0);
-            		photos.add(photos.get(2).nextPhoto());
-            		new GetRemoteImageTask().execute(photos.get(2).getPhotoFull(), 2);
-            	}
-            }
-        }*/
         
         @Override
         public Object instantiateItem(View collection, int position) {
             ImageView image = new ImageView(PhotoPager.this);
-            images.add(position, image);
+            images[position] = image;
+            Bitmap bm = mMemoryCache.get("photo"+position);
+            if(bm != null) {
+            	images[position].setImageBitmap(bm);
+                ((ViewPager) collection).addView(images[position],0);  
+            	return images[position];
+            }
+            images[position] = image;
             new GetRemoteImageTask().execute(photos.get(position).getPhotoFull(), position);
-            ((ViewPager) collection).addView(images.get(position),0);  
-            return images.get(position);
+            ((ViewPager) collection).addView(images[position],0);  
+            return images[position];
         }
         
         @Override
@@ -125,7 +124,8 @@ public class PhotoPager extends Activity {
             }
 
             protected void onPostExecute(Bitmap image) {
-            	images.get(index).setImageBitmap(image);
+            	mMemoryCache.put("photo"+index, image);
+            	images[index].setImageBitmap(image);
             }
         }
     }
