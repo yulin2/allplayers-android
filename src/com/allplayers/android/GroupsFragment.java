556870110ecce5cf051e97f0cc75a78b69e0e9b5
Jonathan Darling <jdarling@allplayers.com>
2013-06-25 15:28:29 -0500

    Beautified GroupsFragment.java.

--- src/com/allplayers/android/GroupsFragment.java
+++ src/com/allplayers/android/GroupsFragment.java
@@ -17,6 +17,9 @@ import android.widget.ProgressBar;
 import com.allplayers.objects.GroupData;
 import com.allplayers.rest.RestApiV1;
 
+/**
+ * Fragment displaying the user's groups.
+ */
 public class GroupsFragment extends ListFragment {
     private ArrayList<GroupData> mGroupList = new ArrayList<GroupData>();
     private boolean hasGroups = false, loadMore = true;
@@ -29,7 +32,13 @@ public class GroupsFragment extends ListFragment {
     private int mAmountToLoad = 15;
     private ListView mListView;
 
-    /** Called when the activity is first created. */
+    /**
+     * Called to do initial creation of a fragment. This is called after onAttach(Activity) and
+     * before onCreateView(LayoutInflater, ViewGroup, Bundle).
+     * 
+     * @param savedInstanceState If the fragment is being re-created from a previous saved state,
+     * this is the state.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,13 +57,19 @@ public class GroupsFragment extends ListFragment {
         // Load the first set of groups.
         new GetUserGroupsTask().execute();
     }
-
+    
+    /**
+     * Called immediately after onCreateView(LayoutInflater, ViewGroup, Bundle) has returned, but
+     * before any saved state has been restored in to the view.
+     */
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        
         // Set up our loading indicator at the foot of the ListView.
         mProgressBar = new ProgressBar(mParentActivity);
         mListView = getListView();
         mListView.addFooterView(mProgressBar, null, false);
+        
         // Set our adapter.
         setListAdapter(mAdapter);
 
@@ -64,6 +79,17 @@ public class GroupsFragment extends ListFragment {
             private int visibleThreshold = 2;
             private int previousTotal = 1;
             private boolean loading = true;
+            
+            /**
+             * Callback method to be invoked when the list or grid has been scrolled. This will be
+             * called after the scroll has completed
+             * 
+             * @param view The view whose scroll state is being reported.
+             * @param firstVisibleItem The index of the first visible cell (ignore if
+             * visibleItemCount == 0).
+             * @param visibleItemCount The number of visible cells.
+             * @param totalItemCount The number of items in the list adaptor.
+             */
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem,
             int visibleItemCount, int totalItemCount) {
@@ -78,27 +104,52 @@ public class GroupsFragment extends ListFragment {
                     loading = true;
                 }
             }
+            
+            /**
+             * Callback method to be invoked while the list view or grid view is being scrolled. If
+             * the view is being scrolled, this method will be called before the next frame of the
+             * scroll is rendered. In particular, it will be called before any calls to getView(int,
+             * View, ViewGroup).
+             * 
+             * @param view The view whose scroll state is being reported.
+             * @param scrollState The current scroll state. One of SCROLL_STATE_IDLE,
+             * SCROLL_STATE_TOUCH_SCROLL or SCROLL_STATE_IDLE.
+             */
             @Override
-            public void onScrollStateChanged(AbsListView arg0, int arg1) {
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                // We don't need this.
             }
-
         });
     }
 
+    /**
+     * This method will be called when an item in the list is selected. Subclasses should override.
+     * Subclasses can call getListView().getItemAtPosition(position) if they need to access the data
+     * associated with the selected item.
+     * 
+     * @param l The ListView where the click happened.
+     * @param v The view that was clicked within the ListView.
+     * @param position The position of the view in the list.
+     * @param id The row id of the item that was clicked.
+     */
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         if (hasGroups && position < mGroupList.size()) {
+            
             //Display the group page for the selected group
             Intent intent = (new Router(mParentActivity)).getGroupPageActivityIntent(mGroupList.get(position));
             startActivity(intent);
         }
     }
 
-    /** Populates the list of groups to display to the UI thread. */
+    /**
+     * Populates the list of groups to display. 
+     */
     protected void updateGroupData() {
         if (!mGroupList.isEmpty()) {
             mAdapter.notifyDataSetChanged();
+            
             // If we did not load 15 groups, we are at the end of the list, so signal
             // not to try to load more groups.
             if (mGroupList.size() - mCurrentAmountShown < mAmountToLoad) {
@@ -107,9 +158,9 @@ public class GroupsFragment extends ListFragment {
                     mListView.removeFooterView(mProgressBar);
                 }
             }
-
             hasGroups = true;
         } else {
+            
             // If we do not have any groups, create a blank list item and remove our loading footer.
             ArrayAdapter<String> blankAdapter = new ArrayAdapter<String>(mParentActivity, android.R.layout.simple_list_item_1);
             blankAdapter.add("No groups to display");
@@ -125,19 +176,33 @@ public class GroupsFragment extends ListFragment {
      * Fetches the groups a user belongs to and stores the data locally.
      */
     public class GetUserGroupsTask extends AsyncTask<Void, Void, String> {
+        
+        /**
+         * Performs a calculation on a background thread. Gets a user's groups from the API.
+         * 
+         * @return The result of the call to get the user's groups.
+         */
         @Override
         protected String doInBackground(Void... args) {
             return RestApiV1.getUserGroups(mPageNumber++ * mAmountToLoad, mAmountToLoad, mSortType);
         }
 
+        /**
+         * Runs on the UI thread after doInBackground(Params...). The specified result is the value
+         * returned by doInBackground(Params...).
+         * 
+         * @param jsonResult The result of the call to get the user's groups.
+         */
         @Override
         protected void onPostExecute(String jsonResult) {
             if (!jsonResult.equals("error")) {
+                
                 // Create the new group data objects and add them to our list.
                 GroupsMap groups = new GroupsMap(jsonResult);
                 mCurrentAmountShown = mGroupList.size();
                 mGroupList.addAll(groups.getGroupData());
             }
+            
             // Update the ListView.
             updateGroupData();
         }
