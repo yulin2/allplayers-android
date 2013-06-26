5e0561ec40783044f6bc9c315b0ad822925171fe
Jonathan Darling <jdarling@allplayers.com>
2013-06-26 10:47:57 -0500

    Beautified MessageThread.java.

--- src/com/allplayers/android/MessageThread.java
+++ src/com/allplayers/android/MessageThread.java
@@ -26,44 +26,55 @@ import com.allplayers.rest.RestApiV1;
 import com.devspark.sidenavigation.SideNavigationView;
 import com.devspark.sidenavigation.SideNavigationView.Mode;
 
+/**
+ * Displays the messages in a message thread.
+ */
 public class MessageThread extends AllplayersSherlockListActivity {
+    
     private ArrayList<MessageThreadData> mMessageThreadList;
-    private boolean hasMessages = false;
-    private String mJsonResult = "";
-    private int mThreadId;
     private ArrayList<HashMap<String, String>> mInfoList = new ArrayList<HashMap<String, String>>(2);
     private MessageData mMessage;
     private ProgressBar mLoadingIndicator;
     private SimpleAdapter mAdapter;
 
+    private boolean hasMessages = false;
+    private int mThreadId;
+    private String mJsonResult = "";
+
     /**
-     * Called when the activity is first created, this sets up some variables,
-     * creates the Action Bar, and sets up the Side Navigation Menu.
-     * @param savedInstanceState: Saved data from the last instance of the
-     * activity.
+     * Called when the activity is starting.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut
+     * down then this Bundle contains the data it most recently supplied in
+     * onSaveInstanceState(Bundle). Otherwise it is null.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.message_thread);
+        
+        // Grab the message thread ID from the intent.
         mMessage = (new Router(this)).getIntentMessage();
         String threadID = mMessage.getThreadID();
 
-        setContentView(R.layout.message_thread);
+        // Set up the loading indicator.
         mLoadingIndicator = (ProgressBar) findViewById(R.id.progress_indicator);
 
-
+        // Set up the Actionbar.
         mActionBar.setTitle("Messages");
 
+        // Set up the Side Navigation Menu.
         mSideNavigationView = (SideNavigationView)findViewById(R.id.side_navigation_view);
         mSideNavigationView.setMenuItems(R.menu.side_navigation_menu);
         mSideNavigationView.setMenuClickCallback(this);
         mSideNavigationView.setMode(Mode.LEFT);
 
+        // Grab the thread's messages.
         new PutAndGetMessagesTask().execute(threadID);
 
         // It has been found that deleting a message through the API completely obliterates it from
         // the site. This is not expected functionality and needs to be researched more.
+        // TODO Figure out why the API does this. It's not very nice.
         /*getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> arg0, View view, final int position, long arg3) {
@@ -87,15 +98,15 @@ public class MessageThread extends AllplayersSherlockListActivity {
     }
 
     /**
-     * Listener for the list on the page.
-     * @param l
-     * @param v
-     * @param position: The position in the list of the clicked item.
-     * @param id: The id of the list item that was clicked.
+     * This method will be called when an item in the list is selected.
+     * 
+     * @param l The ListView where the click happened.
+     * @param v The view that was clicked within the ListView.
+     * @param position: The position of the view in the list.
+     * @param id: The row id of the item that was clicked.
      */
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-
         super.onListItemClick(l, v, position, id);
 
         if (hasMessages) {
@@ -110,6 +121,14 @@ public class MessageThread extends AllplayersSherlockListActivity {
      */
     public class PutAndGetMessagesTask extends AsyncTask<String, Void, String> {
 
+        /**
+         * Performs a calculation on the background thread. Since we have accessed this thread, we
+         * need to mark it as "read".
+         * 
+         * @param threadID The ID of the current thread.
+         * @return The result of getting the thread's messages from the API.
+         */
+        @Override 
         protected String doInBackground(String... threadID) {
 
             mThreadId = Integer.parseInt(threadID[0]);
@@ -121,10 +140,10 @@ public class MessageThread extends AllplayersSherlockListActivity {
         }
 
         /**
-         * Gets a user's sent and received messages and puts this data into the
-         * user's message thread.
-         * @param jsonResult: The json result containing the data for the user's
-         * sent and received messages in this thread.
+         * Runs on the UI thread after doInBackground(Params...). The specified result is the value
+         * returned by doInBackground(Params...).
+         * 
+         * @param jsonResult: Result of fetching the thread's messages.
          */
         protected void onPostExecute(String jsonResult) {
 
@@ -134,6 +153,7 @@ public class MessageThread extends AllplayersSherlockListActivity {
 
             mActionBar.setSubtitle("Thread started by " + mMessageThreadList.get(0).getSenderName());
 
+            // Sort the messages by time. 
             Collections.sort(mMessageThreadList, new Comparator<Object>() {
                 public int compare(Object o1, Object o2) {
                     MessageThreadData m1 = (MessageThreadData) o1;
@@ -142,6 +162,7 @@ public class MessageThread extends AllplayersSherlockListActivity {
                 }
             });
 
+            // Populate the list items with message data.
             if (!mMessageThreadList.isEmpty()) {
                 hasMessages = true;
                 for (int i = 0; i < mMessageThreadList.size(); i++) {
@@ -169,6 +190,12 @@ public class MessageThread extends AllplayersSherlockListActivity {
         }
     }
 
+    /**
+     * Deletes a message from the thread.
+     * 
+     * TODO Currently there is an issue in the API where individual message deletion has some
+     * unwanted effects, these need to be fixed before single message deletion can be implemented.
+     */
     public class DeleteMessageTask extends AsyncTask<Void, Void, String> {
         private int position;
 
